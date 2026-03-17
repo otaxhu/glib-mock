29b96b6b0c71c975101aad2eca0e28090ff41537
otaxhu <oscarperniamoreno@gmail.com>
2026-03-17 13:35:06 -0400

    Initial IAT hooking implementation for windows

--- src/glib-mock-win32.c
+++ src/glib-mock-win32.c
@@ -62,22 +62,34 @@ __declspec(naked) static void *_ReturnAddress (void) { __asm mov eax, [ebp+4] __
 
 /* }}} */
 
-void
-_g_mock_abort (const gchar *func_name, ...);
-
 #define G_WIN32_API_FAILED(func_name) \
-  _g_mock_abort ("GLib-Mock (glib-mock-win32.c): Unexpected error from Win32 API during <%s> call: '%ld' error code", \
-                 # func_name, \
-                 GetLastError ())
+  g_error ("Unexpected error from Win32 API during <" \
+           # func_name \
+           "> call: '%ld' error code", \
+           GetLastError ())
 
 #define G_WIN32_API_FAILED_NO_CODE(func_name) \
-  _g_mock_abort ("GLib-Mock (glib-mock-win32.c): Unexpected error from Win32 API during <%s> call", \
-                 # func_name)
+  g_error ("Unexpected error from Win32 API during <" \
+           # func_name \
+           "> call")
+
+typedef struct
+{
+  gpointer func;
+  const gchar *func_name;
+  gboolean applied;
+} GMockEntry;
+
+static gboolean committed = FALSE;
+static GArray *mock_entries = NULL;
 
 /* We cannot inline because we require the real return address from the caller */
 G_NO_INLINE void
 _g_mock_get_real_win32(gpointer func, const gchar *func_name, gpointer *out_real)
 {
+  if G_UNLIKELY (committed)
+    g_error ("Unexpected call to g_mock_get_real after g_mock_commit has been called");
+
   /* Nothing to do, user is allowed to pass NULL */
   if (!out_real)
     return;
@@ -85,10 +97,6 @@ _g_mock_get_real_win32(gpointer func, const gchar *func_name, gpointer *out_real
   HMODULE exe_module, caller_module;
 
   /* Get the real implementation of the mock */
-
-  exe_module = GetModuleHandle (NULL);
-  if (!exe_module)
-    G_WIN32_API_FAILED (GetModuleHandle);
 
   if (!GetModuleHandleEx (GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
                           GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
@@ -104,15 +112,13 @@ _g_mock_get_real_win32(gpointer func, const gchar *func_name, gpointer *out_real
   if (!EnumProcessModules (process, NULL, 0, &modules_size))
     G_WIN32_API_FAILED (EnumProcessModules);
 
-  HMODULE *modules = (HMODULE *) malloc (modules_size);
-  if (!modules)
-    _g_mock_abort ("GLib-Mock (glib-mock-win32.c): malloc call failed, insufficient memory");
+  HMODULE *modules = (HMODULE *) g_malloc (modules_size);
 
   if (!EnumProcessModules (process, modules, modules_size, &modules_size2))
     G_WIN32_API_FAILED (EnumProcessModules);
 
   if (modules_size != modules_size2)
-    _g_mock_abort ("GLib-Mock (glib-mock-win32.c): EnumProcessModules returned different sizes of modules on the second call");
+    g_error ("EnumProcessModules returned different sizes of modules on the second call");
 
   for (int i = 0; i < modules_size / sizeof (HMODULE); i++)
     {
@@ -129,8 +135,115 @@ _g_mock_get_real_win32(gpointer func, const gchar *func_name, gpointer *out_real
         }
     }
 
-  _g_mock_abort ("GLib-Mock (glib-mock-win32.c): No real implementation found for <%s> mock function", func_name);
+  g_error ("No real implementation found for <%s> mock function", func_name);
 
 out:
-  free (modules);
+  g_free (modules);
+}
+
+void
+_g_mock_add_win32 (gpointer func, const gchar *func_name)
+{
+  if G_UNLIKELY (committed)
+    g_error ("Unexpected call to g_mock_add after g_mock_commit has been called");
+
+  if G_UNLIKELY (!mock_entries)
+    mock_entries = g_array_new (FALSE, FALSE, sizeof (GMockEntry));
+
+  g_array_append_val (mock_entries,
+                      (GMockEntry){
+                        .func = func,
+                        .func_name = func_name,
+                      });
+}
+
+/* We cannot inline, caller's real return address is required */
+G_NO_INLINE void
+g_mock_commit (void)
+{
+  if G_UNLIKELY (committed)
+    return;
+
+  committed = TRUE;
+
+  if G_UNLIKELY (!mock_entries)
+    return;
+
+  DWORD modules_size, modules_size2;
+  HANDLE process = GetCurrentProcess ();
+  if (!process)
+    G_WIN32_API_FAILED (GetCurrentProcess);
+
+  if (!EnumProcessModules (process, NULL, 0, &modules_size))
+    G_WIN32_API_FAILED (EnumProcessModules);
+
+  HMODULE *modules = (HMODULE *) g_malloc (modules_size);
+
+  if (!EnumProcessModules (process, modules, modules_size, &modules_size2))
+    G_WIN32_API_FAILED (EnumProcessModules);
+
+  if (modules_size != modules_size2)
+    g_error ("EnumProcessModules returned different sizes of modules on the second call");
+
+  for (int i = 0; i < modules_size / sizeof (HMODULE); i++)
+    {
+      guintptr base_addr = (guintptr)modules[i];
+
+      IMAGE_DOS_HEADER *dos_header = (IMAGE_DOS_HEADER *)base_addr;
+
+      IMAGE_NT_HEADERS *nt_headers = (IMAGE_NT_HEADERS *)(base_addr + dos_header->e_lfanew);
+      IMAGE_DATA_DIRECTORY import_dir = nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+      if (import_dir.VirtualAddress == 0)
+        continue;
+
+      IMAGE_IMPORT_DESCRIPTOR *import_desc = (IMAGE_IMPORT_DESCRIPTOR *)(base_addr + import_dir.VirtualAddress);
+
+      while (import_desc->Name)
+        {
+          IMAGE_THUNK_DATA *name_thunk = (IMAGE_THUNK_DATA *)(base_addr + import_desc->OriginalFirstThunk);
+          IMAGE_THUNK_DATA *addr_thunk = (IMAGE_THUNK_DATA *)(base_addr + import_desc->FirstThunk);
+
+          while (name_thunk->u1.AddressOfData)
+            {
+              /* We can't hook into ordinal imports */
+              if (!(name_thunk->u1.Ordinal & IMAGE_ORDINAL_FLAG))
+                {
+                  IMAGE_IMPORT_BY_NAME *import_by_name = (IMAGE_IMPORT_BY_NAME *)(base_addr + name_thunk->u1.AddressOfData);
+                  const gchar *imp_name = (const gchar *)import_by_name->Name;
+
+                  for (guint i = 0; i < mock_entries->len; i++)
+                    {
+                      MockEntry *entry = &g_array_index (mock_entries, MockEntry, i);
+
+                      if (g_strcmp0 (imp_name, entry->func_name) == 0)
+                        {
+                          DWORD old_protect;
+                          if (!VirtualProtect (&addr_thunk->u1.Function, sizeof (gpointer), PAGE_READWRITE, &old_protect))
+                            G_WIN32_API_FAILED (VirtualProtect);
+
+                          addr_thunk->u1.Function = (ULONG_PTR)entry->mock_func;
+                          VirtualProtect (&addr_thunk->u1.Function, sizeof (gpointer), old_protect, &old_protect);
+
+                          entry->applied = TRUE;
+                        }
+                    }
+                }
+              name_thunk++;
+              addr_thunk++;
+            }
+          import_desc++;
+        }
+    }
+
+  /* Check mocks were applied */
+
+  for (guint i = 0; i < mock_entries->len; i++)
+    {
+      GMockEntry *entry = &g_array_index (mock_entries, GMockEntry, i);
+
+      if (!entry->applied)
+        g_warning ("Mock <%s> couldn't be applied, you may not see it working correctly", entry->func_name);
+    }
+
+  g_clear_pointer (&mock_entries, g_array_unref);
 }
